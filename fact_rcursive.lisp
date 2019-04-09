0c3fdbf69337a6650354ec13b701e28599089b59
Ignissen <38163224+Ignissen@users.noreply.github.com>
2019-04-09 15:14:46 +0200

    Faktoriális rekurzívan LISP

---
+++ fact_rcursive.lisp
@@ -0,0 +1,13 @@
+
+(defun factorial(x)
+        (if (= x 1)
+            (setq a 1)
+        )
+        (if (> x 1)
+            (setq a (* x (factorial (- x 1))))
+        )
+        
+        a
+)
+
+(format t "~D! is ~D" 5 (factorial 5))
