2b89e8b0cb621f80d5d5e65d8689a509af59b3ed
Ignissen <jozsefpetrus99@gmail.com>
2019-04-15 09:33:51 +0000

    Keras deep dream

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -1,12 +1,6 @@
 import tensorflow as tf
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-import matplotlib.pyplot as plt
-#%matplotlib inline # Only use this if using iPython
-image_index = 7777 # You may select anything up to 60,000
-print(y_train[image_index]) # The label is 8
-plt.imshow(x_train[image_index], cmap='Greys')
-
 x_train.shape
 
 # Reshaping the array to 4-dims so that it can work with the Keras API
